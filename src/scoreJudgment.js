a472b10b69d9d1c191588df18f4d6ceaa87af633
Kazuki Kondo <k.kazuki526@gmail.com>
2022-05-08 17:16:15 +0900

    スコアの計算を行うクラスを作成

---
+++ src/scoreJudgment.js
@@ -0,0 +1,28 @@
+export class scoreJudgment {
+  constructor(hands) {
+    this.hands = hands
+  }
+
+  score() {
+    const hands = this.hands
+
+    if (hands.length === 0) { return 0 }
+
+    const handValues = hands.map((card) => {
+      return card.value
+    })
+
+    let aceCount = handValues.filter((value) => value === 11 ).length
+
+    let totalScore = handValues.reduce((sum, element) => {
+      return sum + element
+    })
+
+    while (totalScore > 21 && aceCount > 0) {
+      totalScore-= 10
+      aceCount--
+    }
+
+    return totalScore
+  }
+}
