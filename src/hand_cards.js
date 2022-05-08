bd608573d1f4f44eb3c9e8e62dc49e1b118eebe6
Kazuki Kondo <k.kazuki526@gmail.com>
2022-05-08 17:15:54 +0900

    カードが正しく表示されるようにコンポーネントを修正

--- src/hand_cards.js
+++ src/hand_cards.js
@@ -4,8 +4,8 @@ export class HandCards extends React.Component {
   render() {
     return this.props.hands.map((card) => {
       return(
-        <span style={{ color: this.props.deck.suitColor(card.suit) }}>
-          { card.displayCode }
+        <span style={{ color: this.props.deck.suitColor(card) }} key={card.displayCode}>
+          { String.fromCodePoint(card.displayCode) }
         </span>
       )
     })
