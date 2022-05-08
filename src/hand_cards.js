f69bf4ee86e74fb318dafac00470ca2b5cfe0cf3
Kazuki Kondo <k.kazuki526@gmail.com>
2022-05-08 16:07:15 +0900

    カード情報の扱い変更に合わせて表示するコンポーネントを変更する

--- src/hand_cards.js
+++ src/hand_cards.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { Card } from "./card"
 
 export class HandCards extends React.Component {
   render() {
-    return this.props.hands.map((hand) => {
-      return <Card hand={hand} deck={this.props.deck} key={Object.values(hand)} />
+    return this.props.hands.map((card) => {
+      return(
+        <span style={{ color: this.props.deck.suitColor(card.suit) }}>
+          { card.displayCode }
+        </span>
+      )
     })
   }
 }
